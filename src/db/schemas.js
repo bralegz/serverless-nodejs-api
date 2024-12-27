85ea86a46bb735287b3d39db5e2770f2aadd4bc0
bralegz <bralegz@gmail.com>
2024-12-27 14:25:50 -0500

    migrations setup

--- src/db/schemas.js
+++ src/db/schemas.js
@@ -1,11 +1,11 @@
 //database schema of leads table
-const { serial } = require('drizzle-orm/mysql-core');
+const { serial } = require('drizzle-orm/pg-core');
 const { text, pgTable, timestamp } = require('drizzle-orm/pg-core');
 
 const LeadTable = pgTable('leads', {
   id: serial('id').primaryKey().notNull(),
   email: text('email').notNull(),
-  description: text('description').default('This is my comment'),
+  // description: text('description').default('This is my comment'),
   createdAt: timestamp('created_at').defaultNow()
 });
 
